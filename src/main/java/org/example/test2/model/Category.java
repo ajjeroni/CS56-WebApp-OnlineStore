2681d1dbe94e6c7141e14f1902a08b72218f8675
Adan <adanjeronimo213@gmail.com>
2024-12-03 22:49:13 -0800

    New files and structure

---
+++ src/main/java/org/example/test2/model/Category.java
@@ -0,0 +1,33 @@
+package org.example.test2.model;
+
+import java.util.List;
+
+public abstract class Category {
+    private String categoryName;
+    private String categoryDesc;
+    private List<Product> products;
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String name) {
+        this.categoryName = name;
+    }
+
+    public String getCategoryDesc() {
+        return categoryDesc;
+    }
+
+    public void setCategoryDesc(String desc) {
+        this.categoryDesc = desc;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+}
---
+++ src/main/java/org/example/test2/model/Customer.java
@@ -0,0 +1,28 @@
+package org.example.test2.model;
+
+public class Customer {
+    private String name; // main.java.com.test.store.model.Customer's name
+    private double balance; // main.java.com.test.store.model.Customer's balance
+
+    public Customer(String name) {
+        this.name = name;
+        this.balance = 0;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBalance(double sum) {
+        this.balance = sum;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
